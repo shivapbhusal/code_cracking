06bb02985cdb6d64e32db9890178b6138f243abe
shivapbhusal <civabhusal@gmail.com>
2018-01-25 18:41:15 -0500

    add string compression

--- Arrays_Strings/1.6/string_compression.py
+++ Arrays_Strings/1.6/string_compression.py
@@ -5,18 +5,26 @@ def compress_string(string):
     i=0
     final_str=''
     current=mylist[0]
-
     count=0
-    while (i<len(string)-1):
+
+    for i in range(len(mylist)):
         if mylist[i]!=current:
             final_str=final_str+str(current)+str(count) 
             current=mylist[i]
-            count=0
-            i=i+1
+            count=1
         else:
             count=count+1
-            i=i+1
+            if i==(len(mylist)-1):
+                final_str=final_str+str(current)+str(count)
 
     return final_str 
 
-print(compress_string('aaaaabbcccdeee'))
+def return_compression (string):
+    if len(string)<len(compress_string(string)):
+        return string 
+    else:
+        return compress_string(string)
+
+print(return_compression('aaaaabbcccdeee'))
+print(return_compression('abc'))
+print(return_compression('aaibc'))
