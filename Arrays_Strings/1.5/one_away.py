fa618ccbc518631a2a418f7b2df62b7a5c9dbed4
shivapbhusal <civabhusal@gmail.com>
2018-01-24 23:04:10 -0500

    add 1.5 -- check one away

---
+++ Arrays_Strings/1.5/one_away.py
@@ -0,0 +1,40 @@
+# A program to test whether two strings are one or zero edits away 
+
+def check_string (str1, str2):
+
+    list1=list(str1)
+    list2=list(str2)
+
+    if abs(len(list1)-len(list2))>1:
+        return 'NO'
+
+    else:
+        return check_list(list1, list2)
+
+
+def check_list(list1, list2):
+    if len(list1)>len(list2):
+
+        for elements in list2:
+            if elements not in list1:
+                return 'NO' 
+        return 'YES' 
+
+    elif list1==list2:
+        return 'YES' 
+
+    else:
+        for elements in list1:
+            if elements not in list2:
+                return 'NO' 
+        return 'YES'
+       
+
+#Test cases 
+print(check_string('abc','def')) #NO
+
+print(check_string('abc','ab')) #YES 
+
+print(check_string('abcd','abc')) #YES 
+
+print(check_string('dcba','abc')) #NO
