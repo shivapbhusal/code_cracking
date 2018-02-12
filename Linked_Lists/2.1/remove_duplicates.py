00883690a0fa69032b898b0398369cef07244236
shivapbhusal <civabhusal@gmail.com>
2018-02-11 22:42:47 -0500

    remove duplicates using temp buffer

--- Linked_Lists/2.1/remove_duplicates.py
+++ Linked_Lists/2.1/remove_duplicates.py
@@ -4,6 +4,15 @@ sys.path.insert(0,'/home/civaist/Documents/Others/code_cracking/Linked_Lists')
 
 from linked_list import LinkedList
 
+def insertUnique(data,uniqueList):
+	current=uniqueList.head
+	while (current):
+		if current.get_data()==data:
+			return None
+		current=current.get_next() 
+	uniqueList.insert(data)
+	
+
 L=LinkedList()
 
 for i in range(10):
@@ -12,22 +21,17 @@ for i in range(10):
 for i in range(5):
 	L.insert(i)
 
-finalList=LinkedList()
+uniqueList=LinkedList()
 
-current1=L.head
-current2=L.head 
-while (current1):
-	count=1
-	while(current2):
-		if current1==current2:
-			count=count+1
-		current2=current2.get_next()
-	if count>1:
-		finalList.insert(current1.get_data())
+current=L.head 
+while (current):
+	insertUnique(current.get_data(),uniqueList)
+	current=current.get_next()
 
-	current1=current1.get_next()
+uniqueList.read()
 
-finalList.read()
+
+
 
 
 
