69330018182702fbd5feb1207c70d1dcf4f97500
shivapbhusal <civabhusal@gmail.com>
2018-02-11 21:19:13 -0500

    add draft-remove duplicate

--- Linked_Lists/2.1/remove_duplicates.py
+++ Linked_Lists/2.1/remove_duplicates.py
@@ -9,4 +9,27 @@ L=LinkedList()
 for i in range(10):
     L.insert(i)
 
-L.read()
+for i in range(5):
+	L.insert(i)
+
+finalList=LinkedList()
+
+current1=L.head
+current2=L.head 
+while (current1):
+	count=1
+	while(current2):
+		if current1==current2:
+			count=count+1
+		current2=current2.get_next()
+	if count>1:
+		finalList.insert(current1.get_data())
+
+	current1=current1.get_next()
+
+finalList.read()
+
+
+
+
+
